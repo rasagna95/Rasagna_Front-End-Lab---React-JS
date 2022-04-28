5c016894454665327e064f50590f44f884dccc15
rasagna95 <rasagnarasmi95@gmail.com>
2022-04-28 20:40:11 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import ExpenseTracker from "./components/ ExpenseTracker"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as React from "react";
+
+function App() {
+    return (
+        <div>
+            <ExpenseTracker />
+        </div>
+    );
+}
+
+export default App;
